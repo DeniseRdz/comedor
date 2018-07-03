ff1ab87f7c54a1b00a8e432e8f32f61678b277e8
Oscar Armando Recio Delgado <oscar.recio@neoris.cxnetworks.net>
2018-07-03 14:42:29 -0600

    Todo listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { AboutComponent } from './about/about.component';
+import { CartComponent } from './cart/cart.component';
+import { HistorialComponent } from './historial/historial.component';
+import { RegisterComponent } from './register/register.component';
+import { PlatilloComponent } from './platillo/platillo.component';
+import { NavbarComponent } from './navbar/navbar.component';
+const appRoutes: Routes = [
+  {path: '', component: LogInComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'shoppingcart', component: CartComponent},
+  {path: 'historial', component: HistorialComponent},
+  {path: 'login', component: LogInComponent},
+  {path: 'register', component: RegisterComponent},
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LogInComponent,
+    AboutComponent,
+    CartComponent,
+    HistorialComponent,
+    RegisterComponent,
+    PlatilloComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
