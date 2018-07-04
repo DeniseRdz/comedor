2d4a0d29a01941c25a1d54189d628e2dfa7200b4
Oscar Armando Recio Delgado <oscar.recio@neoris.cxnetworks.net>
2018-07-04 16:38:31 -0600

    Home con Service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
+import { GarrisonService } from './../service/garrison/garrison.service';
+import { DrinkService } from './../service/drink/drink.service';
+import { StewService } from './../service/stew/stew.service';
 import { Component, OnInit } from '@angular/core';
+import { IGarrisonModel } from '../interface/garrison';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  meal: any;
+  platiloAll = [];
+  stewsArray: any;
+  garrisonsArray: any;
+  drinksArray: any;
+
+  constructor(public stewService: StewService, public drinkService: DrinkService,
+    public garrisonService: GarrisonService) {
+      this.prepareMeal();
+     }
 
   ngOnInit() {
+  }
+
+  prepareMeal() {
+    const streamStew = this.stewService.getAllStew();
+    streamStew.valueChanges().subscribe((list) => {
+     this.stewsArray = list;
+    });
+
+    const streamDrink = this.drinkService.getAllDrinks();
+    streamDrink.valueChanges().subscribe((list) => {
+      this.drinksArray = list;
+    });
+
+    const streamGarrison = this.garrisonService.getAllGarrison();
+    streamGarrison.valueChanges().subscribe((list) => {
+      this.garrisonsArray = list;
+      this.concat();
+    });
+  }
+
+  concat() {
+    for (let i = 0; i < this.stewsArray.length; i++) {
+        this.meal = {
+          stew: this.stewsArray[i],
+          drink: this.drinksArray[i],
+          garrison: this.garrisonsArray[i]
+        };
+
+        this.platiloAll.push(this.meal);
+    }
+    console.log(this.platiloAll);
   }
 
 }
