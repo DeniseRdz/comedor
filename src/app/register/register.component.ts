109cb3297f97c109b25243b59bdcc86d3bf17579
Diego Alfonso Serna Rivera <diego.serna@neoris.cxnetworks.net>
2018-07-04 13:32:43 -0500

    update component html and ts log-in & register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthenticationService } from '../service/authentication/authentication.service';
+import { UserService } from '../service/user/user.service';
+import { Router } from '@angular/router';
+import { IUserModel } from '../interface/user';
+
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +13,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  //#region Binding ngModel
+  name:string;
+  lastName:string;
+  email:string;
+  password:string;
+  passwordCheck:string;
+  //#endregion
+  constructor(public router: Router, public authenticationService: AuthenticationService, public userService :UserService) { }
 
   ngOnInit() {
   }
 
+  Register(){
+    if(this.password === this.passwordCheck){
+
+      const promise = this.authenticationService.emailRegistration(this.email,this.password);
+      promise.then((data) =>{
+        alert("Registrado con éxito");
+
+        var ObjectRegister : IUserModel = 
+        {
+          age : 0,
+          avatar : "",
+          favDish :"",
+          gender:"",
+          lastName: this.lastName,
+          mail:this.email,
+          name: this.name,
+          userId:data.user.uid,
+         };
+
+        this.addUser(ObjectRegister);
+        //this.router.navigateByUrl("/home");
+    
+      }).catch((error)=>{
+        alert("Valio Riel");
+        console.log(error);
+      });
+
+    }
+    else
+    {
+      alert("La contraseña no coincide");
+    }
+
+  }
+
+  addUser(user){
+
+
+    const promise = this.userService.createUser(user);
+    promise.then(() =>{
+      alert("Usuario Creado Exito!");
+    }).catch( (error) =>{
+      alert("hubo pedo!");
+      console.log(error);
+    });
+  
+  
+  //#region Binding ngModel " "
+  this.name="";
+  this.lastName="";
+  this.email="";
+  this.password="";
+  this.passwordCheck="";
+  //#endregion
+  
+    this.router.navigateByUrl("/login");
+  }
+  
 }
