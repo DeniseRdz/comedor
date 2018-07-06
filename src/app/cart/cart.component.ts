df1684210b4763fe04f84fe30d651b8fada5cd4b
isaac.gutierrez <isaac_gutierrez@hotmail.com>
2018-07-06 14:45:26 -0500

    Modal credit card

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { isNumber } from 'util';
 
 @Component({
   selector: 'app-cart',
@@ -10,8 +11,15 @@ export class CartComponent implements OnInit {
 
   step=1;
   closeResult: string;
+  // hero:any;
+  valueName:string='';
+  vNameMsg:string ='';
   
-  constructor(private modalService: NgbModal) { }
+  valueNumber:number=0;
+  vNumberMsg:string ='';
+  
+  constructor(private modalService: NgbModal) {
+   }
 
   open(content) {
     // this.modalService.open(content);
@@ -19,9 +27,42 @@ export class CartComponent implements OnInit {
       // alert(`Pago Realizado`);
       if(this.step==2){
       this.closeResult = `Pago Realizado`;}
-  //   }, (reason) => {
-  //     this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
+  }
+
+  
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
+  validateName(){
+    if( this.valueName.length == 0  ){
+      this.vNameMsg = "Ingresar un nombre valido"
+    }else{
+      this.vNameMsg ='';
+      this.step = 3;
+      
+    }
+    
+  }
+
+  
+  validateNumber(){
+    if( this.valueNumber.toString().length == 0  ){
+      this.vNumberMsg = "Ingresar un numero valido";
+    }else{
+      this.vNumberMsg ='';
+      this.step = 3;
+    }
   }
 
   ngOnInit() {
